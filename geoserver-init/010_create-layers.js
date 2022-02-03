32aba857f607b2389150937bde1c713a2ac46261
Jakob Miksch <jakob@meggsimum.de>
2022-02-03 16:56:09 +0100

    Add GeoServer with init script

---
+++ geoserver-init/010_create-layers.js
@@ -0,0 +1,47 @@
+import GeoServerRestClient from 'geoserver-node-client';
+
+const geoserverUrl = process.env.GEOSERVER_REST_URL || 'http://localhost:8080/geoserver/rest/';
+
+const geoserverDefaultUser = 'admin';
+const geoserverDefaultPw = 'geoserver';
+
+const workspaceOverlay = 'klips';
+const workspaceOverLayUri = 'https://www.meggsimum.de/namespace/klips';
+
+/**
+ * Main function
+ */
+async function initGeoserver () {
+  await createWorkspaces();
+}
+
+/**
+ * Create the workspaces.
+ */
+async function createWorkspaces () {
+  console.log();
+  console.log('### WORKSPACES ###');
+  console.log();
+
+  const wsOverLayerExists = await grc.namespaces.get(workspaceOverlay);
+  if (wsOverLayerExists) {
+    console.log(`INFO: workspace '${workspaceOverlay}' already exists'`);
+  } else {
+    const workspaceOverlayCreated = await grc.namespaces.create(workspaceOverlay, workspaceOverLayUri);
+    if (workspaceOverlayCreated) {
+      console.log(`SUCCESS: Created Workspace: '${workspaceOverlay}'`);
+    } else {
+      console.error(`ERROR: Creation of Workspace failed '${workspaceOverlay}'`);
+    }
+  }
+}
+
+// check if we can connect to GeoServer REST API
+const grc = new GeoServerRestClient(geoserverUrl, geoserverDefaultUser, geoserverDefaultPw);
+grc.exists().then(gsExists => {
+  if (gsExists === true) {
+    initGeoserver();
+  } else {
+    console.error('ERROR: Could not connect to GeoServer REST API - ABORT!');
+  }
+});
