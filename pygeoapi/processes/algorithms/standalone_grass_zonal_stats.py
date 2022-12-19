0649eda579e25f735fb6a44f728e55ba0591f8c0
blitza@terrrestris.de <blitza@terrestris.de>
2022-12-19 15:32:42 +0100

    update docs and add multiple geoms support

--- pygeoapi/processes/algorithms/standalone_grass_zonal_stats.py
+++ pygeoapi/processes/algorithms/standalone_grass_zonal_stats.py
@@ -6,10 +6,15 @@ import csv
 import json
 from argparse import ArgumentParser
 
+# Prepare GRASS session
+# cf. https://grasswiki.osgeo.org/wiki/GRASS_Python_Scripting_Library
+# cf. https://grasswiki.osgeo.org/wiki/Working_with_GRASS_without_starting_it_explicitly
 gisbase = subprocess.check_output(["grass", "--config", "path"], text=True).strip()
 
+# Set GISBASE environment variable
 os.environ['GISBASE'] = gisbase
 
+# Python path: we ask GRASS GIS where its Python packages are
 sys.path.append(
     subprocess.check_output(["grass", "--config", "python_path"], text=True).strip()
 )
@@ -22,6 +27,12 @@ from grass.pygrass.modules.shortcuts import vector as v
 
 
 def generate_zonal_stats(rastermap, geometries):
+    """
+    Creates zonal statistics from a COG using a single polygon as input
+    :param rastermap: the URL of the COG
+    :param polygon_geojson: A single polygon as dict structured as GeoJSON in the same projection as the COG
+    :returns A dict structured JSON containing the zonal statistics as properties
+    """
     with tempfile.TemporaryDirectory() as tempdir:
         with open(f'{tempdir}/output', 'w') as output:
             location = f'{tempdir}/grass'
@@ -52,11 +63,13 @@ def generate_zonal_stats(rastermap, geometries):
                 v.to_rast(input=polygon_name, output=zone_name,
                           use='val', value=i)
                 r.univar(map=demo_raster_name, zones=zone_name,
-                         output=output_file, separator='comma', flags='t')
+                         output=output_file, separator='comma', flags='t', overwrite=True)
 
                 with open(output_file, 'r') as input:
-                    for line in input:
-                        output.write(line)
+                    for index, line in enumerate(input):
+                        # only write header once
+                        if not (i > 0 and index == 0):
+                            output.write(line)
 
                 g.remove(type='vector', name=polygon_name, flags='f')
                 g.remove(type='raster', name=zone_name, flags='f')
@@ -66,21 +79,20 @@ def generate_zonal_stats(rastermap, geometries):
             result = result.readlines()
 
         # format result to JSON
-        # TODO: only works for one polygon and is a quick fix that should changed
         keys = []
         values = []
         with open(f'{tempdir}/output', 'r') as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=',')
             i = 0
-            for row in csv_reader:
-                if i == 0:
+            for rowNumber, row in enumerate(csv_reader):
+                if rowNumber == 0:
                     keys = row
-                    i = i + 1
-                if i == 1:
-                    values = row
-        formatted_output = dict(zip(keys, values))
+                else:
+                    values.append(row)
 
-    outputs = formatted_output
+            outputs = []
+            for value in values:
+                outputs.append(dict(zip(keys, value)))
 
     return outputs
 
@@ -100,7 +112,6 @@ def main():
     with open(args.geometries) as json_data:
         results = generate_zonal_stats(rastermap=args.cog, geometries=json.load(json_data))
         print(results)
-
 
 if __name__ == '__main__':
     main()
