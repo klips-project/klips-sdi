6ce02dceed3bcbc6daa704c8e52f7bbc396a425f
Jakob Miksch <jakob@meggsimum.de>
2023-01-16 16:02:49 +0100

    Fix demo.py

--- pygeoapi/processes/demo.py
+++ pygeoapi/processes/demo.py
@@ -1,15 +1,12 @@
 """Demo to test alogrithms."""
 
-from location_info import (
-    get_available_cog_file_names,
-    get_location_info_time,
-    get_location_info
-)
+from algorithms.location_info import get_location_info, get_location_info_time
+from algorithms.util import get_available_cog_file_names, url_exists
 
 if __name__ == '__main__':
     cog_dir_url = 'http://localhost/cog/dresden/dresden_temperature/'
     cog_list = get_available_cog_file_names(cog_dir_url)
-    # print(cog_list)
+    print(cog_list)
 
     x = 4582606.6
     y = 3115558.3
@@ -18,4 +15,5 @@ if __name__ == '__main__':
 
     cog_url = 'http://localhost/cog/dresden/dresden_temperature/dresden_20221127T1000Z.tif'  # noqa
 
-    print(get_location_info(cog_url, x, y))
+    if (url_exists(cog_url)):
+        print(get_location_info(cog_url, x, y))
