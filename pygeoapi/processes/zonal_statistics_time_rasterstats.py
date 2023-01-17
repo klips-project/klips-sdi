c7cc8d80c9548dde62809dbd1538b9a690adfc14
Jakob Miksch <jakob@meggsimum.de>
2023-01-17 08:59:41 +0100

    WIP: create boilerplate process

---
+++ pygeoapi/processes/zonal_statistics_time_rasterstats.py
@@ -0,0 +1,157 @@
+# =================================================================
+#
+# Authors: Tom Kralidis <tomkralidis@gmail.com>
+#
+# Copyright (c) 2019 Tom Kralidis
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# =================================================================
+
+import logging
+from pygeoapi.process.base import BaseProcessor, ProcessorExecuteError
+from .algorithms.zonal_stats import get_zonal_stats
+
+LOGGER = logging.getLogger(__name__)
+
+#: Process metadata and description
+PROCESS_METADATA = {
+    'version': '0.1.0',
+    'id': 'zonal-statistics-time-rasterstats',
+    'title': {
+        'en': 'TODO Zonal statistics of a COG using rasterstats',
+        'de': 'TODO Zonale Statistiken einer COG-Datei mit Hilfe von rasterstas'
+    },
+    'description': {
+        'en': 'TODO Compute zonal statistics of a subset of a public accessible COG. Only queries data of the first raster band.',
+        'de': 'TODO Berechnet zonale Statistiken eines öffentlich zugänglichen COGs. Fragt nur Daten vom ersten Rasterband ab.'
+    },
+    'keywords': ['rasterstats', 'zonal statistics'],
+    'links': [],
+    'inputs': {
+        'TODO cogUrl': {
+            'title': 'The URL of the COG',
+            'description': 'The public available URL of the COG to query',
+            'schema': {
+                'type': 'string'
+            },
+            'minOccurs': 1,
+            'maxOccurs': 1,
+        },
+        'TODO polygonGeojson': {
+            'title': 'Polygon GeoJSON',
+            'description': 'A polygon GeoJSON for which to compute zonal statistics of the COG',
+            'minOccurs': 1,
+            'maxOccurs': 1,
+            'schema': {
+                '$ref': 'http://schemas.opengis.net/ogcapi/features/part1/1.0/openapi/schemas/geometryGeoJSON.json'
+            }
+        },
+        'TODO statisticMethods': {
+            'title': 'Statistical Methods',
+            'description': 'The statistical methods to apply. Any out of:  [\'count\', \'min\', \'max\', \'mean\', \'sum\', \'std\', \'median\', \'majority\', \'minority\', \'unique\', \'range\', \'nodata\', \'nan\']',
+            'maxOccurs': 1,
+            'schema': {
+                'type': 'Array'
+            }
+        }
+    },
+    'outputs': {
+        'values': {
+            'title': 'TODO',
+            'description': 'TODO',
+            'schema': {
+                'type': 'string'
+            }
+        }
+    },
+    'example': {
+        "inputs": {
+            "cogUrl": "https://example.com/sample-cog.tif",
+            "statisticMethods": ["count", "majority"],
+            "polygonGeojson": {
+                "coordinates": [
+                    [
+                        [
+                            7.398378066263234,
+                            50.35623617599114
+                        ],
+                        [
+                            7.398378066263234,
+                            47.310703221840384
+                        ],
+                        [
+                            14.007896257545866,
+                            47.310703221840384
+                        ],
+                        [
+                            14.007896257545866,
+                            50.35623617599114
+                        ],
+                        [
+                            7.398378066263234,
+                            50.35623617599114
+                        ]
+                    ]
+                ],
+                "type": "Polygon"
+            }
+        }
+    }
+}
+
+
+class ZonalStatisticsTimeRasterstatsProcessor(BaseProcessor):
+
+    def __init__(self, processor_def):
+        super().__init__(processor_def, PROCESS_METADATA)
+
+    def execute(self, data):
+
+        cog_url = data.get('cogUrl')
+        polygon_geojson = data.get('polygonGeojson')
+        statistic_methods = data.get('statisticMethods')
+        # TODO: ensure polygon is not too large, otherwise process takes very long or even crashes
+
+        result = None
+
+        if statistic_methods:
+            result = get_zonal_stats(
+                cog_url,
+                polygon_geojson,
+                statistic_methods=statistic_methods
+            )
+        else:
+            result = get_zonal_stats(
+                cog_url,
+                polygon_geojson
+            )
+
+        outputs = {
+            'values': result
+        }
+
+        mimetype = 'application/json'
+        return mimetype, outputs
+
+    def __repr__(self):
+        return '<ZonalStatisticsTimeRasterstatsProcessor> {}'.format(self.name)
