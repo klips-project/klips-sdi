00b0bd58337cd30bd2d4d2f56f5b66b52e8446f0
Jakob Miksch <jakob@meggsimum.de>
2023-01-27 13:46:28 +0100

    Use same logging for klips-api

--- klips-api/src/logger/index.ts
+++ klips-api/src/logger/index.ts
@@ -1,13 +1,27 @@
 import pino from 'pino';
+import pretty from 'pino-pretty';
+import path from 'path';
+import fs from 'fs';
 
-export const logger = pino({
-  level: process.env.LOG_LEVEL || 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'yyyy-mm-dd HH:MM:ssZ',
-      ignore: 'pid,hostname'
-    }
-  }
-});
+const logFilePath = process.env.LOG_FILE_PATH || './logs/worker.log';
+
+// create directory of log file if it does not exist
+if (!fs.existsSync(logFilePath)) {
+  const dir = path.dirname(logFilePath);
+  fs.mkdirSync(dir, { recursive: true });
+}
+
+const logLevel: any = process.env.LOG_LEVEL || 'debug';
+
+export const logger = pino(
+  {
+    level: logLevel
+  },
+  pino.multistream(
+    [
+      { stream: pretty({ colorize: true }), level: logLevel },
+      { stream: pino.destination(logFilePath), level: logLevel },
+    ]
+  )
+);
+
