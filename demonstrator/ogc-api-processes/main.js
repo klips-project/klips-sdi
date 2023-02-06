cea99ced2e6d3bd75e1a946e5124e14c7cdf9d12
Jakob Miksch <jakob@meggsimum.de>
2023-02-06 14:03:27 +0100

    Document demonstrator

--- demonstrator/ogc-api-processes/main.js
+++ demonstrator/ogc-api-processes/main.js
@@ -1,25 +1,39 @@
+// The relative path to the URL of the respective OGC API Processes instance
 const oapiProcessesUrl = "../processes/";
-let processId = '';
-let draw;
 
+// The chosen Process ID
+let processId = '';
+
+// The OpenLayers Draw interaction
+let olDraw;
+
+// The EPSG code of Webmercator
+const epsg3857 = 'EPSG:3857';
+
+/**
+ * Adjusts the app to one chosen process.
+ *
+ * @param {String} process The id/name of the process
+ */
 const setProcess = (process) => {
+  // set global variable processId
   processId = process;
-  map.removeInteraction(draw);
+  map.removeInteraction(olDraw);
   let type = 'Polygon';
   if (process === 'location-info-rasterstats' || process === 'location-info-time-rasterstats') {
     type = 'Point';
   }
-  draw = new ol.interaction.Draw({
-    source: source,
+  olDraw = new ol.interaction.Draw({
+    source: drawSource,
     type
   });
-  map.addInteraction(draw);
+  map.addInteraction(olDraw);
 
-  draw.on('drawstart', () => {
-    source.clear();
+  olDraw.on('drawstart', () => {
+    drawSource.clear();
   });
-  
-  draw.on('drawend', (event) => {
+
+  olDraw.on('drawend', (event) => {
     const feature3857 = event.feature;
     let x;
     let y;
@@ -29,7 +43,7 @@ const setProcess = (process) => {
     }
 
     const formatGeoJson = new ol.format.GeoJSON({
-      featureProjection: 'EPSG:3857'
+      featureProjection: epsg3857
     });
     const geoJsonGeom = formatGeoJson.writeGeometry(feature3857.getGeometry());
 
@@ -37,72 +51,77 @@ const setProcess = (process) => {
     switch (processId) {
       case 'hello-world':
         payload = {
-          "inputs": {
-            "name": "Peter"
+          inputs: {
+            name: "Peter"
           }
         };
         break;
       case 'zonal-statistics-grass':
         payload = {
-          "inputs": {
-            "inputGeometries": [{
-              "value": JSON.parse(geoJsonGeom),
-              "mediaType": "application/geo+json"
+          inputs: {
+            inputGeometries: [{
+              value: JSON.parse(geoJsonGeom),
+              mediaType: "application/geo+json"
             }],
-            "cogUrl": "http://nginx/cog/dresden_20220216T1146Z.tif"
+            cogUrl: "http://nginx/cog/dresden_20220216T1146Z.tif"
           }
         };
         break;
       case 'zonal-statistics-rasterstats':
         payload = {
-          "inputs": {
-            "polygonGeoJson": JSON.parse(geoJsonGeom),
-            "cogUrl": "http://nginx/cog/dresden_20220216T1146Z.tif",
-            "statisticMethods": ['count', 'min', 'max', 'mean', 'sum', 'std', 'median', 'majority', 'minority', 'unique', 'range', 'nodata', 'nan']
+          inputs: {
+            polygonGeoJson: JSON.parse(geoJsonGeom),
+            cogUrl: "http://nginx/cog/dresden_20220216T1146Z.tif",
+            statisticMethods: ['count', 'min', 'max', 'mean', 'sum', 'std', 'median', 'majority', 'minority', 'unique', 'range', 'nodata', 'nan']
           }
         };
         break;
       case 'zonal-statistics-time-rasterstats':
         payload = {
-          "inputs": {
-            "polygonGeoJson": JSON.parse(geoJsonGeom),
-            "inputCrs": "EPSG:4326",
-            "cogDirUrl": "http://nginx/cog/dresden/dresden_temperature/",
-            "startTimeStamp": "2000-10-02T12:32:00Z",
-            "endTimeStamp": "2024-10-08T12:32:00Z"
+          inputs: {
+            polygonGeoJson: JSON.parse(geoJsonGeom),
+            inputCrs: epsg3857,
+            cogDirUrl: "http://nginx/cog/dresden/dresden_temperature/",
+            startTimeStamp: "2000-10-02T12:32:00Z",
+            endTimeStamp: "2024-10-08T12:32:00Z"
           }
         };
         break;
       case 'location-info-rasterstats':
         payload = {
-          "inputs": {
-            "x": x,
-            "y": y,
-            "inputCrs": "EPSG:3857",
-            "cogUrl": "http://nginx/cog/dresden/dresden_temperature/dresden_20221008T1232Z.tif"
+          inputs: {
+            x: x,
+            y: y,
+            inputCrs: epsg3857,
+            cogUrl: "http://nginx/cog/dresden/dresden_temperature/dresden_20221008T1232Z.tif"
           }
         };
         break;
       case 'location-info-time-rasterstats':
         payload = {
-          "inputs": {
-            "x": x,
-            "y": y,
-            "cogDirUrl": "http://nginx/cog/dresden/dresden_temperature/",
-            "inputCrs": "EPSG:3857",
-            "startTimeStamp": "2000-01-01T12:32:00Z",
-            "endTimeStamp": "2024-12-31T12:32:00Z"
+          inputs: {
+            x: x,
+            y: y,
+            cogDirUrl: "http://nginx/cog/dresden/dresden_temperature/",
+            inputCrs: epsg3857,
+            startTimeStamp: "2000-01-01T12:32:00Z",
+            endTimeStamp: "2024-12-31T12:32:00Z"
           }
         };
         break;
       default:
         console.error('unknown process: ', processId);
       }
-  
+
     requestOapiProcesses(oapiProcessesUrl, payload)
   })
 }
 
+/**
+ * Creates a chart from the results of a process.
+ *
+ * @param {Object} json The result of the process
+ */
 const setupChart = (json) => {
   const el = document.getElementById('chart');
   let chart = echarts.getInstanceByDom(el);
@@ -143,13 +162,13 @@ fetch(oapiProcessesUrl + "?f=json")
     })
     .catch(error => console.log('error', error));
 
-const raster = new ol.layer.Tile({
+const osmBasemap = new ol.layer.Tile({
   source: new ol.source.OSM(),
 });
 
-const source = new ol.source.Vector();
-const vector = new ol.layer.Vector({
-  source: source,
+const drawSource = new ol.source.Vector();
+const drawLayer = new ol.layer.Vector({
+  source: drawSource,
   style: {
     'fill-color': 'rgba(255, 255, 255, 0.7)',
     'stroke-color': 'black',
@@ -161,7 +180,7 @@ const vector = new ol.layer.Vector({
 
 
 const map = new ol.Map({
-  layers: [raster, vector],
+  layers: [osmBasemap, drawLayer],
   target: 'map',
   view: new ol.View({
     center: [1528647, 6631453],
@@ -169,17 +188,20 @@ const map = new ol.Map({
   }),
 });
 
-
+/**
+ * Request a OGC API Process and display results.
+ *
+ * @param {String} url The URL of the OGC API Process
+ * @param {Object} payload The payload to send to the process
+ */
 const requestOapiProcesses = (url, payload) => {
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-
-  const raw = JSON.stringify(payload);
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
 
   const requestOptions = {
     method: 'POST',
-    headers: myHeaders,
-    body: raw,
+    headers: headers,
+    body: JSON.stringify(payload),
     redirect: 'follow'
   };
 
