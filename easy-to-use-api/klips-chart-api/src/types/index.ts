2084b2254d31513d46c4cb2ea42afd815d7ffc3a
sdobbert <sdobbert@terrestris.de>
2023-05-31 16:58:06 +0200

    general chart configuration

--- easy-to-use-api/klips-chart-api/src/types/index.ts
+++ easy-to-use-api/klips-chart-api/src/types/index.ts
@@ -8,3 +8,10 @@ export type Params = {
 export type PathNameConfig = {
   [pathName: string]: string;
 };
+
+export type TimeSeriesDatapoint = {
+ 'band_1': string;
+ 'timestamp': string;
+};
+
+export type TimeSeriesData = TimeSeriesDatapoint[];
