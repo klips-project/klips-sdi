3820a61103ad793fcb80a6583d7f834ef6d61550
blitza@terrrestris.de <blitza@terrestris.de>
2023-06-12 13:53:20 +0200

    Several fixes chart api config

--- easy-to-use-api/klips-chart-api/src/main.ts
+++ easy-to-use-api/klips-chart-api/src/main.ts
@@ -6,38 +6,41 @@ import { validateParams } from './util/Config';
 
 import './style.css';
 
-try {
-  // parse params
-  let params;
-  if (document.location.search) {
-    params = getParams('search');
-  } else if (document.location.pathname.length > 1) {
-    params = parseURLPathnames(document.location.pathname, pathNameConfig);
+// use top-level async function to make use of await
+(async () => {
+  try {
+    // parse params
+    let params;
+    if (document.location.search) {
+      params = getParams('search');
+    } else if (document.location.pathname.length > 1) {
+      params = parseURLPathnames(document.location.pathname, pathNameConfig);
+    }
+
+    // validate params
+    if (!params || !validateParams(params)) {
+      throw new Error('Invalid url params.');
+    }
+
+    // get dom
+    const domEl = document.querySelector('#chart');
+
+    if (!domEl) {
+      throw new Error('Could not find target element in dom.');
+    }
+
+    const chartApi = await ChartAPI.getChartData(params);
+
+    if (chartApi) {
+      chartApi.render();
+    }
+  } catch (error) {
+    console.error(error);
+    const errorElement: HTMLElement | null = document.querySelector('#error');
+
+    if (errorElement) {
+      errorElement.style.display = 'block';
+      errorElement.textContent = 'An unexpected error has occured. Please check the console.';
+    }
   }
-
-  // validate params
-  if (!params || !validateParams(params)) {
-    throw new Error('Invalid url params.');
-  }
-
-  // get dom
-  const domEl = document.querySelector('#chart');
-
-  if (!domEl) {
-    throw new Error('Could not find target element in dom.');
-  }
-
-  const chartApi = await ChartAPI.getChartData(params);
-
-  if (chartApi) {
-    chartApi.render();
-  }
-} catch (error) {
-  console.error(error);
-  const errorElement: HTMLElement | null = document.querySelector('#error');
-
-  if (errorElement) {
-    errorElement.style.display = 'block';
-    errorElement.textContent = 'An unexpected error has occured. Please check the console.';
-  }
-}
+})();
