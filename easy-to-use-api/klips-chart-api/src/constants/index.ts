b8bbc87b938bb3ca3a53600f4ec960367b3d12ae
blitza@terrrestris.de <blitza@terrestris.de>
2023-06-29 13:39:08 +0200

    revert process url

--- easy-to-use-api/klips-chart-api/src/constants/index.ts
+++ easy-to-use-api/klips-chart-api/src/constants/index.ts
@@ -7,7 +7,6 @@ export const pathNameConfig: PathNameConfig = {
   path4: 'threshold'
 };
 
-export const processURL = 'https://klips-dev.terrestris.de/processes/location-info-time-rasterstats/execution';
-//  window.location.href.indexOf('localhost') > -1 ?
-  // 'http://localhost:5000/processes/location-info-time-rasterstats/execution/' :
-  // 'https://klips-dev.terrestris.de/processes/location-info-time-rasterstats/execution';
+export const processURL = window.location.href.indexOf('localhost') > -1 ?
+  'http://localhost:5000/processes/location-info-time-rasterstats/execution/' :
+  'https://klips-dev.terrestris.de/processes/location-info-time-rasterstats/execution';
