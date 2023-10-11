2e0cdf38272afd0d572e3fdfd6e52295cac7d19a
sdobbert <dobbert@terrestris.de>
2023-10-11 12:36:20 +0200

    fix internal paths

--- easy-to-use-api/klips-warning-api/src/components/GetData/index.tsx
+++ easy-to-use-api/klips-warning-api/src/components/GetData/index.tsx
@@ -1,9 +1,4 @@
-// @ts-ignore
-import WKTParser from 'jsts/org/locationtech/jts/io/WKTParser';
-// @ts-ignore
-import GeoJSONWriter from 'jsts/org/locationtech/jts/io/GeoJSONWriter';
 import { Params } from '../../types';
-import params from '../../Url';
 
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc.js';
@@ -16,7 +11,7 @@ const endDate = dayjs().add(49, 'hours').utc().format('YYYY-MM-DDTHH:00:00Z');
 const processURL = 'https://klips-dev.terrestris.de/processes/location-info-time-rasterstats/execution';
 const processURLPolygon = 'https://klips-dev.terrestris.de/processes/zonal-statistics-time-rasterstats/execution';
 
-const fetchTemperaturePoint = async (
+export const fetchTemperaturePoint = async (
     params: Params,
     coordinates: any
 ): Promise<any> => {
@@ -44,10 +39,10 @@ const fetchTemperaturePoint = async (
         throw new Error(`HTTP error status: ${response.status}`);
     }
 
-    return response.json();
+    return await response.json();
 };
 
-const fetchTemperaturePolygon = async (
+export const fetchTemperaturePolygon = async (
     params: Params,
     coordinates: any
 ): Promise<any> => {
@@ -75,25 +70,5 @@ const fetchTemperaturePolygon = async (
         throw new Error(`HTTP error status: ${response.status}`);
     }
 
-    return await response.json().then(response => { return response });
+    return await response.json();
 };
-
-// get WKT
-const wktReader = new WKTParser();
-const wktGeometry = wktReader.read(params.geom);
-
-// get GeoJSON
-const geoJSONWriter = new GeoJSONWriter();
-const geoJSONGeometry = geoJSONWriter.write(wktGeometry);
-
-// get data
-let dataSeries: any;
-if (params.geom?.includes('POLYGON')) {
-   // dataSeries = await fetchTemperaturePolygon(params, geoJSONGeometry).then(response => { return response });
-} else {
-   // dataSeries = await fetchTemperaturePoint(params, wktGeometry.getCoordinates()[0]).then(response => { return response });
-};
-
-const temperature = dataSeries.values;
-
-export default temperature;
