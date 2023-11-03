386586417992f746f8364c38a214b723622a0c6f
Fritz Hoeing <fhoeing@terrestris.de>
2023-11-03 15:54:43 +0100

    lint: fix spaces

--- easy-to-use-api/klips-url-generator/src/components/WarningComponent/index.tsx
+++ easy-to-use-api/klips-url-generator/src/components/WarningComponent/index.tsx
@@ -12,159 +12,159 @@ import DrawGeometry from "../DrawGeometry";
 import SelectInfo from "../SelectInfo";
 
 export type Threshold = {
-    green: String,
-    orange: String,
-    red: String
+  green: String,
+  orange: String,
+  red: String
 }
 
 export interface WarningProps {
-    geoJsonGeom: string,
-    region: string;
-    wktGeom: string;
-    onDrawEnd?: (geom: OlGeometry) => void;
-    onDrawStart?: (event: OlDrawEvent) => void;
+  geoJsonGeom: string,
+  region: string;
+  wktGeom: string;
+  onDrawEnd?: (geom: OlGeometry) => void;
+  onDrawStart?: (event: OlDrawEvent) => void;
 };
 
 export type WarningComponentProps = WarningProps;
 
 const WarningComponent: React.FC<WarningComponentProps> = ({ geoJsonGeom, region, wktGeom, onDrawEnd, onDrawStart }) => {
-    const [band, setBand] = useState('physical');
-    const [threshold, setThreshold] = useState<Threshold>({ green: '0', orange: '30', red: '35' });
-    const [url, setURL] = useState('');
-    const [info, setInfo] = useState<string>('info-board');
+  const [band, setBand] = useState('physical');
+  const [threshold, setThreshold] = useState<Threshold>({ green: '0', orange: '30', red: '35' });
+  const [url, setURL] = useState('');
+  const [info, setInfo] = useState<string>('info-board');
 
-    useEffect(() => {
-        if (region && wktGeom && threshold && band) {
-            setURL(`https://klips-dev.terrestris.de/easy-to-use-api/warning/?region=${region.toLowerCase()}&geom=${wktGeom}&thresholdgreen=${threshold.green}&thresholdorange=${threshold.orange}&thresholdred=${threshold.red}&band=${band}&format=${info}`)
-        };
-    }, [region, wktGeom, threshold, band, info]);
-
-    const changeBand = (newBand: string) => {
-        setBand(newBand);
+  useEffect(() => {
+    if (region && wktGeom && threshold && band) {
+      setURL(`https://klips-dev.terrestris.de/easy-to-use-api/warning/?region=${region.toLowerCase()}&geom=${wktGeom}&thresholdgreen=${threshold.green}&thresholdorange=${threshold.orange}&thresholdred=${threshold.red}&band=${band}&format=${info}`)
     };
-    
-    const changeInfo = (newInfo: string) => {
-        setInfo(newInfo);
-    };
+  }, [region, wktGeom, threshold, band, info]);
 
-    const changeThreshold = (key: keyof Threshold) => (newThreshold: string) => {
-        let newState = {
-            ...threshold
-        };
+  const changeBand = (newBand: string) => {
+    setBand(newBand);
+  };
 
-        newState[key] = newThreshold;
+  const changeInfo = (newInfo: string) => {
+    setInfo(newInfo);
+  };
 
-        setThreshold(newState);
+  const changeThreshold = (key: keyof Threshold) => (newThreshold: string) => {
+    let newState = {
+      ...threshold
     };
 
-    const onMailClick = () => {
-        if (!url) {
-            return;
-        }
-        const mailSubject = 'Widget-URL';
-        const mailBody = `Hey,\r\nnutz doch diese URL für das Widget:\r\n\r\n${url}`;
+    newState[key] = newThreshold;
 
-        const mailToUrl = new URL('mailto:');
-        mailToUrl.searchParams.set('subject', mailSubject);
-        mailToUrl.searchParams.set('body', mailBody);
-        window.open(mailToUrl.toString().replace(/\+/g, '%20'), '_self');
+    setThreshold(newState);
+  };
+
+  const onMailClick = () => {
+    if (!url) {
+      return;
     }
+    const mailSubject = 'Widget-URL';
+    const mailBody = `Hey,\r\nnutz doch diese URL für das Widget:\r\n\r\n${url}`;
 
-    const onTabClick = () => {
-        if (!url) {
-            return;
-        }
-        window.open(url, url)
+    const mailToUrl = new URL('mailto:');
+    mailToUrl.searchParams.set('subject', mailSubject);
+    mailToUrl.searchParams.set('body', mailBody);
+    window.open(mailToUrl.toString().replace(/\+/g, '%20'), '_self');
+  }
+
+  const onTabClick = () => {
+    if (!url) {
+      return;
     }
+    window.open(url, url)
+  }
 
-    return (
-        <>
-            <div className='geometry'>
-                <h3>Geometrie:</h3>
-                <div className='geometry-button'>
-                    <DrawGeometry
-                        drawType='Point'
-                        drawStyle={style.point}
-                        onDrawEnd={onDrawEnd}
-                        onDrawStart={onDrawStart}
-                    />
-                    <DrawGeometry
-                        drawType='Polygon'
-                        drawStyle={style.polygon}
-                        onDrawEnd={onDrawEnd}
-                        onDrawStart={onDrawStart}
-                    />
-                </div>
-                {!geoJsonGeom ? <></> :
-                    <div className='geom-textarea'>
-                        <TextArea
-                            readOnly
-                            value={geoJsonGeom}
-                        />
-                        <Tooltip
-                            title='Copy GeoJSON'
-                        >
-                            <CopyOutlined onClick={() => onCopyClickGeom(geoJsonGeom)} />
-                        </Tooltip>
-                    </div>
-                }
-            </div>
-            <div className='attributes'>
-                <h3>Band:</h3>
-                <SelectBand
-                    inputBands={optionsBand.slice(0, -1)}
-                    changeBand={changeBand}
-                    selectedBand={band}
-                />
-                <h3>Grenzwert:</h3>
-                <h4>Grüne Warnung:</h4>
-                <SelectThreshold
-                    changeThreshold={changeThreshold('green')}
-                />
-                <h4>Orangene Warnung:</h4>
-                <SelectThreshold
-                    changeThreshold={changeThreshold('orange')}
-                />
-                <h4>Rote Warnung:</h4>
-                <SelectThreshold
-                    changeThreshold={changeThreshold('red')}
-                />
-            </div>
-            <SelectInfo
-                selectedInfo={info}
-                changeInfo={changeInfo}
+  return (
+    <>
+      <div className='geometry'>
+        <h3>Geometrie:</h3>
+        <div className='geometry-button'>
+          <DrawGeometry
+            drawType='Point'
+            drawStyle={style.point}
+            onDrawEnd={onDrawEnd}
+            onDrawStart={onDrawStart}
+          />
+          <DrawGeometry
+            drawType='Polygon'
+            drawStyle={style.polygon}
+            onDrawEnd={onDrawEnd}
+            onDrawStart={onDrawStart}
+          />
+        </div>
+        {!geoJsonGeom ? <></> :
+          <div className='geom-textarea'>
+            <TextArea
+              readOnly
+              value={geoJsonGeom}
             />
-            <div className='permalink-component'>
-                <h3>URL:</h3>
-                <div className="permalink">
-                    <Input
-                        readOnly
-                        value={url}
-                    />
-                    <div className="url-icons">
-                        <Tooltip
-                            title='URL in Zwischenablage Kopieren'>
-                            <CopyOutlined onClick={() => onCopyClickUrl(url)} />
-                        </Tooltip>
-                        <Tooltip
-                            title='URL als E-Mail versenden'>
-                            <MailOutlined onClick={onMailClick} />
-                        </Tooltip>
-                        <Tooltip
-                            title='URL in einem neuen Tab öffnen'>
-                            <PlusCircleOutlined onClick={onTabClick} />
-                        </Tooltip>
-                    </div>
-                    <h3>IFrame:</h3>
-                    <TextArea
-                        rows={4}
-                        readOnly
-                        value={url ? `<iframe id="inlineFrameExample" title="Warnung" width="90%" height="700px" src="${url}"></iframe>` : ''}
-                    />
-                </div>
-            </div>
-        </>
-    )
+            <Tooltip
+              title='Copy GeoJSON'
+            >
+              <CopyOutlined onClick={() => onCopyClickGeom(geoJsonGeom)} />
+            </Tooltip>
+          </div>
+        }
+      </div>
+      <div className='attributes'>
+        <h3>Band:</h3>
+        <SelectBand
+          inputBands={optionsBand.slice(0, -1)}
+          changeBand={changeBand}
+          selectedBand={band}
+        />
+        <h3>Grenzwert:</h3>
+        <h4>Grüne Warnung:</h4>
+        <SelectThreshold
+          changeThreshold={changeThreshold('green')}
+        />
+        <h4>Orangene Warnung:</h4>
+        <SelectThreshold
+          changeThreshold={changeThreshold('orange')}
+        />
+        <h4>Rote Warnung:</h4>
+        <SelectThreshold
+          changeThreshold={changeThreshold('red')}
+        />
+      </div>
+      <SelectInfo
+        selectedInfo={info}
+        changeInfo={changeInfo}
+      />
+      <div className='permalink-component'>
+        <h3>URL:</h3>
+        <div className="permalink">
+          <Input
+            readOnly
+            value={url}
+          />
+          <div className="url-icons">
+            <Tooltip
+              title='URL in Zwischenablage Kopieren'>
+              <CopyOutlined onClick={() => onCopyClickUrl(url)} />
+            </Tooltip>
+            <Tooltip
+              title='URL als E-Mail versenden'>
+              <MailOutlined onClick={onMailClick} />
+            </Tooltip>
+            <Tooltip
+              title='URL in einem neuen Tab öffnen'>
+              <PlusCircleOutlined onClick={onTabClick} />
+            </Tooltip>
+          </div>
+          <h3>IFrame:</h3>
+          <TextArea
+            rows={4}
+            readOnly
+            value={url ? `<iframe id="inlineFrameExample" title="Warnung" width="90%" height="700px" src="${url}"></iframe>` : ''}
+          />
+        </div>
+      </div>
+    </>
+  )
 };
 
 export default WarningComponent;
--- easy-to-use-api/klips-warning-api/App.tsx
+++ easy-to-use-api/klips-warning-api/App.tsx
@@ -15,179 +15,179 @@ import { NotificationInput, ResultObject, ResultThreshold, Params } from './src/
 import { Alert } from 'antd';
 
 const App: React.FC = () => {
-    type PromiseResponse = { response: ResultObject[] };
-    type GeometryObject = {
-        wktGeometry: any,
-        geoJSONGeometry: any
-    };
-    const [warning, setWarning] = useState<NotificationInput>(notificationOptions[0]);
-    const [criticalDate, setCriticalDate] = useState<Date | undefined>();
-    const [params, setParams] = useState<Params | null>(null);
-    const [temperature, setTemperature] = useState<PromiseResponse>({ response: [] });
-    const [geometry, setGeometry] = useState<GeometryObject | null>();
-    const [region, setRegion] = useState<string>('');
-    const [state, setState] = useState<string>('');
-    const [widget, setWidget] = useState<any>(<></>)
+  type PromiseResponse = { response: ResultObject[] };
+  type GeometryObject = {
+    wktGeometry: any,
+    geoJSONGeometry: any
+  };
+  const [warning, setWarning] = useState<NotificationInput>(notificationOptions[0]);
+  const [criticalDate, setCriticalDate] = useState<Date | undefined>();
+  const [params, setParams] = useState<Params | null>(null);
+  const [temperature, setTemperature] = useState<PromiseResponse>({ response: [] });
+  const [geometry, setGeometry] = useState<GeometryObject | null>();
+  const [region, setRegion] = useState<string>('');
+  const [state, setState] = useState<string>('');
+  const [widget, setWidget] = useState<any>(<></>)
 
-    // Get url
-    const url = Linking.useURL();
+  // Get url
+  const url = Linking.useURL();
 
-    useEffect(() => {
-        if (!url) {
-            return;
-        }
+  useEffect(() => {
+    if (!url) {
+      return;
+    }
 
-        const { queryParams } = Linking.parse(url);
-        setParams(queryParams as Params);
-    }, [url]);
+    const { queryParams } = Linking.parse(url);
+    setParams(queryParams as Params);
+  }, [url]);
 
-    useEffect(() => {
-        if (!params) {
-            return;
-        }
+  useEffect(() => {
+    if (!params) {
+      return;
+    }
 
-        setRegion(`${params.region}`);
+    setRegion(`${params.region}`);
 
-        // get WKT anf GeoJSON
-        const wktReader = new WKTParser();
-        const geoJSONWriter = new GeoJSONWriter();
+    // get WKT anf GeoJSON
+    const wktReader = new WKTParser();
+    const geoJSONWriter = new GeoJSONWriter();
 
-        const wktGeometry = wktReader.read(params.geom);
-        const geoJSONGeometry = geoJSONWriter.write(wktGeometry);
+    const wktGeometry = wktReader.read(params.geom);
+    const geoJSONGeometry = geoJSONWriter.write(wktGeometry);
 
-        setGeometry({
-            wktGeometry: wktGeometry,
-            geoJSONGeometry: geoJSONGeometry
+    setGeometry({
+      wktGeometry: wktGeometry,
+      geoJSONGeometry: geoJSONGeometry
+    })
+  }, [params]);
+
+  useMemo(() => {
+    // get data
+    setState('loading');
+
+    if (!region || !geometry) {
+      return;
+    }
+
+    if (params?.geom?.includes('POLYGON')) {
+      fetchTemperaturePolygon(region, geometry.geoJSONGeometry)
+        .then((response) => {
+          setState('success');
+          setTemperature({
+            response: response.values
+          });
         })
-    }, [params]);
+        .catch((error) => {
+          console.log(error)
+        });
+    } else if (params?.geom?.includes('POINT')) {
+      fetchTemperaturePoint(region, geometry.wktGeometry.getCoordinates()[0])
+        .then((response) => {
+          setState('success');
+          setTemperature({
+            response: response.values
+          });
+        })
+        .catch((error) => {
+          console.log(error)
+        });
 
-    useMemo(() => {
-        // get data
-        setState('loading');
+    };
 
-        if (!region || !geometry) {
-            return;
-        }
+  }, [region, geometry]);
 
-        if (params?.geom?.includes('POLYGON')) {
-            fetchTemperaturePolygon(region, geometry.geoJSONGeometry)
-                .then((response) => {
-                    setState('success');
-                    setTemperature({
-                        response: response.values
-                    });
-                })
-                .catch((error) => {
-                    console.log(error)
-                });
-        } else if (params?.geom?.includes('POINT')) {
-            fetchTemperaturePoint(region, geometry.wktGeometry.getCoordinates()[0])
-                .then((response) => {
-                    setState('success');
-                    setTemperature({
-                        response: response.values
-                    });
-                })
-                .catch((error) => {
-                    console.log(error)
-                });
+  useEffect(() => {
+    if (!params) {
+      return;
+    };
 
-        };
+    // get band
+    let band: keyof ResultObject = 'band_1'
+    if (params.band === 'perceived') {
+      band = 'band_2'
+    } else if (params.band === 'difference') {
+      band = 'band_3'
+    };
 
-    }, [region, geometry]);
+    const resultThreshold: ResultThreshold = {
+      green: temperature.response.filter((obj: ResultObject) => {
+        return obj[band] > (params.thresholdgreen!);
+      }),
+      orange: temperature.response.filter((obj: ResultObject) => {
+        return obj[band] > Number(params.thresholdorange!);
+      }),
+      red: temperature.response.filter((obj: ResultObject) => {
+        return obj[band] > Number(params.thresholdred!);
+      }),
+    };
 
-    useEffect(() => {
-        if (!params) {
-            return;
-        };
+    console.log(resultThreshold)
 
-        // get band
-        let band: keyof ResultObject = 'band_1'
-        if (params.band === 'perceived') {
-            band = 'band_2'
-        } else if (params.band === 'difference') {
-            band = 'band_3'
-        };
+    if (resultThreshold.red.length > 0) {
+      setWarning(notificationOptions[3])
+      setCriticalDate(resultThreshold.red[0].timestamp);
+    } else if (resultThreshold.orange.length > 0) {
+      setWarning(notificationOptions[2]);
+      setCriticalDate(resultThreshold.orange[0].timestamp);
+    } else if (resultThreshold.green.length > 0) {
+      setWarning(notificationOptions[1]);
+      setCriticalDate(resultThreshold.green[0].timestamp);
+    };
+  }, [params, temperature]);
 
-        const resultThreshold: ResultThreshold = {
-            green: temperature.response.filter((obj: ResultObject) => {
-                return obj[band] > (params.thresholdgreen!);
-            }),
-            orange: temperature.response.filter((obj: ResultObject) => {
-                return obj[band] > Number(params.thresholdorange!);
-            }),
-            red: temperature.response.filter((obj: ResultObject) => {
-                return obj[band] > Number(params.thresholdred!);
-            }),
-        };
+  useEffect(() => {
+    if (!params) {
+      return;
+    };
 
-        console.log(resultThreshold)
+    if (params.format === 'traffic-light') {
+      switch (warning.name) {
+        case 'blue':
+          setWidget(<img src='./src/resources/traffic-light-red.svg' />)
+          break;
+        case 'orange':
+          setWidget(<img src='./src/resources/traffic-light-yellow.svg' />)
+          break;
+        case 'red':
+          setWidget(<img src='./src/resources/traffic-light-red.svg' />)
+          break;
+        default:
+          setWidget(<img src='./src/resources/traffic-light-green.svg' />)
+      };
+    }
 
-        if (resultThreshold.red.length > 0) {
-            setWarning(notificationOptions[3])
-            setCriticalDate(resultThreshold.red[0].timestamp);
-        } else if (resultThreshold.orange.length > 0) {
-            setWarning(notificationOptions[2]);
-            setCriticalDate(resultThreshold.orange[0].timestamp);
-        } else if (resultThreshold.green.length > 0) {
-            setWarning(notificationOptions[1]);
-            setCriticalDate(resultThreshold.green[0].timestamp);
-        };
-    }, [params, temperature]);
+    if (params.format === 'info-board') {
+      // Get Timeframe
+      const date = new Date();
 
-    useEffect(() => {
-        if (!params) {
-            return;
-        };
+      // Check for consistent thresholds and create widget
+      if (params.thresholdgreen! > params.thresholdorange! ||
+        params.thresholdgreen! > params.thresholdred! ||
+        params.thresholdorange! > params.thresholdred!
+      ) {
+        setWidget(<Alert
+          message="Grenzwerte sind nicht in einer logischen Reihenfolge. Bitte passen Sie die Reihenfolge an."
+          type="error"
+        />)
+      } else {
+        setWidget(<CreateAlert
+          warning={warning}
+          location={params.geom}
+          currentDate={date}
+          band={params.band}
+          criticalDate={criticalDate}
+        />)
+      };
+    }
+  }, [params, warning]);
 
-        if (params.format === 'traffic-light') {
-            switch (warning.name) {
-                case 'blue':
-                    setWidget(<img src='./src/resources/traffic-light-red.svg'/>)
-                    break;
-                case 'orange':
-                    setWidget(<img src='./src/resources/traffic-light-yellow.svg'/>)
-                    break;
-                case 'red':
-                    setWidget(<img src='./src/resources/traffic-light-red.svg'/>)
-                    break;
-                default:
-                    setWidget(<img src='./src/resources/traffic-light-green.svg'/>)
-            };
-        }
-
-        if (params.format === 'info-board') {
-            // Get Timeframe
-            const date = new Date();
-
-            // Check for consistent thresholds and create widget
-            if (params.thresholdgreen! > params.thresholdorange! ||
-                params.thresholdgreen! > params.thresholdred! ||
-                params.thresholdorange! > params.thresholdred!
-            ) {
-                setWidget(<Alert
-                    message="Grenzwerte sind nicht in einer logischen Reihenfolge. Bitte passen Sie die Reihenfolge an."
-                    type="error"
-                />)
-            } else {
-                setWidget(<CreateAlert
-                    warning={warning}
-                    location={params.geom}
-                    currentDate={date}
-                    band={params.band}
-                    criticalDate={criticalDate}
-                />)
-            };
-        }
-    }, [params, warning]);
-
-    return (
-        <View>
-            {state === 'loading' ? (
-                <>Loading...</>
-            ) : <>{widget}</>}
-        </View>
-    );
+  return (
+    <View>
+      {state === 'loading' ? (
+        <>Loading...</>
+      ) : <>{widget}</>}
+    </View>
+  );
 };
 
 export default App;
