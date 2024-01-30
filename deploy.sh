0e93e8f4793d639ad0f217d68d1d5cf24115f9a1
sdobbert <dobbert@terrestris.de>
2024-01-30 13:55:28 +0100

    add workers to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 cd ./klips-sdi
-echo "Pull latest repository changes"
+echo "Pull latest repository changes for klips-sdi"
 git stash
 git pull origin main
 echo "Pull latest images"
 docker-compose pull
+cd ../rabbitmq-worker/
+echo "Pull latest repository changes for rabbitmq-worker"
+git stash
+git pull origin main
+echo "Pull latest images"
+docker-compose pull
+cd ../klips-sdi
 echo "Start containers" 
 docker-compose down
 docker-compose up -d
